a524a117c63c0b9203249a586a9a86b0267b4458
tr3vbr0w <trevbrow@live.com>
2019-08-16 14:50:47 -0600

    updated html to reflect assignment, js next

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,17 @@
+//Firebase data connection configuration here, copied from my online firebase acct called train-scheduler
+var firebaseConfig = {
+    apiKey: "AIzaSyA80NV-qQdr8eYJrtqPtk_nhCDpDXOFP_A",
+    authDomain: "train-scheduler-d0b05.firebaseapp.com",
+    databaseURL: "https://train-scheduler-d0b05.firebaseio.com",
+    projectId: "train-scheduler-d0b05",
+    storageBucket: "train-scheduler-d0b05.appspot.com",
+    messagingSenderId: "47581232379",
+    appId: "1:47581232379:web:547a7408795f79b6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var database = firebase.database()
+  
 $('#submit').click(function(e) {
     e.preventDefault()
     let dateVal = $('#start-date').val()
@@ -17,36 +31,24 @@ $('#submit').click(function(e) {
     roleVal = $('#role').val('')
     monthsWorked = $('#months-worked').val('')
 })
-var firebaseConfig = {
-    apiKey: "AIzaSyBSupGmm6XaufmciwJ1TEnqCC4fQmYfflU",
-    authDomain: "employeedatabase-86c88.firebaseapp.com",
-    databaseURL: "https://employeedatabase-86c88.firebaseio.com",
-    projectId: "employeedatabase-86c88",
-    storageBucket: "employeedatabase-86c88.appspot.com",
-    messagingSenderId: "656950926794",
-    appId: "1:656950926794:web:10f5145248d759b3"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-var database = firebase.database()
 //Each time a new
-database.ref().on("child_added", function(snapshot) {
-    // let totalBilled = monthlyRateVal * monthsWorked;
-    // let currentdate = new Date(year, month, day);
-    console.log(snapshot.val());
-    var r = $('<tr>')
-    var nameTd = $('<td>')
-    var roleTd = $('<td>')
-    var startDateTd = $('<td>')
-    var monthsWorkedTd = $('<td>')
-    var monthlyRateTd = $('<td>')
-        // var totalBilledTd = $('<td>')
-    nameTd.text(snapshot.val().employeeName);
-    roleTd.text(snapshot.val().roleVal);
-    startDateTd.text(snapshot.val().dateVal);
-    monthsWorkedTd.text(snapshot.val().monthsWorked);
-    monthlyRateTd.text(snapshot.val().monthlyRate);
-    // totallBilledTd.text(snapshot.val().totalBilled);
-    $(r).append(nameTd, roleTd, startDateTd, monthsWorkedTd, monthlyRateTd)
-    $('tbody').append(r)
-})
+// database.ref().on("child_added", function(snapshot) {
+//     // let totalBilled = monthlyRateVal * monthsWorked;
+//     // let currentdate = new Date(year, month, day);
+//     console.log(snapshot.val());
+//     var r = $('<tr>')
+//     var nameTd = $('<td>')
+//     var roleTd = $('<td>')
+//     var startDateTd = $('<td>')
+//     var monthsWorkedTd = $('<td>')
+//     var monthlyRateTd = $('<td>')
+//         // var totalBilledTd = $('<td>')
+//     nameTd.text(snapshot.val().employeeName);
+//     roleTd.text(snapshot.val().roleVal);
+//     startDateTd.text(snapshot.val().dateVal);
+//     monthsWorkedTd.text(snapshot.val().monthsWorked);
+//     monthlyRateTd.text(snapshot.val().monthlyRate);
+//     // totallBilledTd.text(snapshot.val().totalBilled);
+//     $(r).append(nameTd, roleTd, startDateTd, monthsWorkedTd, monthlyRateTd)
+//     $('tbody').append(r)
+// })
